942cb6fd962b69f355808db2f418dfc68e39366b
leonardthang <leonard.thang@sjsu.edu>
2019-03-12 00:41:49 -0700

    Added redux to app. Currently have a reducer for fetching products.

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'semantic-ui-css/semantic.min.css';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'semantic-ui-css/semantic.min.css'
+import App from './App'
+import './index.css'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer'
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunk)
+);
 
 /* global document */
 /* eslint-disable react/jsx-filename-extension */
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'))
 registerServiceWorker();
