03530243670d94e83b2dfb5c4fc8efa55f15ec9d
leonardthang <leonard.thang@sjsu.edu>
2019-03-13 11:49:32 -0700

    Added redux dev tool for debugging

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,16 @@ import 'semantic-ui-css/semantic.min.css'
 import App from './App'
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose  } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk';
 import rootReducer from './rootReducer'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk)
+    rootReducer, /* preloadedState, */
+    composeEnhancers(applyMiddleware(thunk)),
 );
 
 /* global document */
