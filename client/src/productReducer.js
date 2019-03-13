b3a7e8d099b0b1c742b14b39702704d20f99c2d1
leonardthang <leonard.thang@sjsu.edu>
2019-03-13 11:48:25 -0700

    Added state for pagination in the product reducer.

--- client/src/productReducer.js
+++ client/src/productReducer.js
@@ -1,13 +1,18 @@
 import {
     FETCH_PRODUCTS_BEGIN,
     FETCH_PRODUCTS_SUCCESS,
-    FETCH_PRODUCTS_FAILURE
+    FETCH_PRODUCTS_FAILURE,
+    CHANGE_OFFSET,
+    CHANGE_PAGE
 } from './productActions';
 
 const initialState = {
     items: [],
     loading: false,
-    error: null
+    error: null,
+    pageCount: 0,
+    offset: 0,
+    page: 0
 };
 
 export default function productReducer(state = initialState, action) {
@@ -23,7 +28,8 @@ export default function productReducer(state = initialState, action) {
             return {
                 ...state,
                 loading: false,
-                items: action.payload.products
+                items: action.payload.products,
+                pageCount: Math.ceil(action.payload.total / 10)
             };
 
         case FETCH_PRODUCTS_FAILURE:
@@ -33,7 +39,16 @@ export default function productReducer(state = initialState, action) {
                 error: action.payload.error,
                 items: []
             };
-
+        case CHANGE_OFFSET:
+            return {
+                ...state,
+                offset: action.payload.offset
+            }
+        case CHANGE_PAGE:
+            return {
+                ...state,
+                page: action.payload.page
+            }
         default:
             return state;
     }
