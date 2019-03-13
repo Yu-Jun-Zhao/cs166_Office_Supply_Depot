ad227086a6a700c51cc923c150467cfbd25bddf1
Yu Jun Zhao <yuj.zhao4@gmail.com>
2019-03-13 12:07:06 -0700

    db on the fly

--- server/server.js
+++ server/server.js
@@ -1,45 +1,61 @@
-import express from 'express'
-import cors from 'cors'
-import mysql from 'mysql'
-import { authenticationRequired, adminAuthenticationRequired } from './AuthenticationMiddleware/AuthenticationMiddleware'
-import bodyparser from 'body-parser'
+import express from "express";
+import cors from "cors";
+import mysql from "mysql";
+import {
+  authenticationRequired,
+  adminAuthenticationRequired
+} from "./AuthenticationMiddleware/AuthenticationMiddleware";
+import bodyparser from "body-parser";
 
 const connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '',
-  database : 'osd',
+  host: "localhost",
+  user: "user",
+  password: "1234@bcd",
+  database: "osd",
   port: 3306
 });
 
-connection.connect()
+// Create database on the fly
+// For Now
+connection.connect(err => {
+  if (err) throw err;
+  connection.query("CREATE DATABASE IF NOT EXISTS osd", (err, result) => {
+    if (err) throw err;
+  });
+});
+
+let sql =
+  "CREATE TABLE product(id INT AUTO_INCREMENT, pName VARCHAR(255), quantity INT, price FLOAT, weight FLOAT, PRIMARY KEY (id))";
+connection.query(sql, (err, result) => {
+  if (err) throw err;
+});
 
 const app = express();
-app.use(bodyparser.json())
+app.use(bodyparser.json());
 
 /**
  * For local testing only!  Enables CORS for all domains
  */
 app.use(cors());
 
-
-app.get('/api/products/:name', (req, res) => {
-  const name = req.params.name
-  const sql = `SELECT * FROM product WHERE pName LIKE '%${name}%'`
+app.get("/api/products/:name", (req, res) => {
+  const name = req.params.name;
+  const sql = `SELECT * FROM product WHERE pName LIKE '%${name}%'`;
   connection.query(sql, (error, results) => {
-    if (error) res.send(error)
-    let arr = []
+    if (error) res.send(error);
+    let arr = [];
     for (let i = 0; i < results.length; i++) {
-      arr.push(results[i])
+      arr.push(results[i]);
     }
-    const json = {products: arr}
-    res.json(json)
-  })
+    const json = { products: arr };
+    res.json(json);
+  });
 });
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    message: 'Hello!  There\'s not much to see here :) Please grab one of our front-end samples for use with this sample resource server'
+    message:
+      "Hello!  There's not much to see here :) Please grab one of our front-end samples for use with this sample resource server"
   });
 });
 
@@ -48,7 +64,7 @@ app.get('/', (req, res) => {
  * will echo the contents of the access token if the middleware successfully
  * validated the token.
  */
-app.get('/secure', authenticationRequired, (req, res) => {
+app.get("/secure", authenticationRequired, (req, res) => {
   res.json(req.jwt);
 });
 
@@ -56,16 +72,16 @@ app.get('/secure', authenticationRequired, (req, res) => {
  * Another example route that requires a valid access token for authentication, and
  * print some messages for the user if they are authenticated
  */
-app.get('/api/messages', authenticationRequired, (req, res) => {
+app.get("/api/messages", authenticationRequired, (req, res) => {
   res.json({
     messages: [
       {
-        date:  new Date(),
-        text: 'I am a robot.'
+        date: new Date(),
+        text: "I am a robot."
       },
       {
-        date:  new Date(new Date().getTime() - 1000 * 60 * 60),
-        text: 'Hello, world!'
+        date: new Date(new Date().getTime() - 1000 * 60 * 60),
+        text: "Hello, world!"
       }
     ]
   });
