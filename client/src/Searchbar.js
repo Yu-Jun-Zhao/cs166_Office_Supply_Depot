c642967c6d88594e8a78c1f9d00bd45ef8fa8a1a
leonardthang <leonard.thang@sjsu.edu>
2019-03-13 11:47:06 -0700

    Changed CSS to only modify suggestions and inactive classNames

--- client/src/Searchbar.js
+++ client/src/Searchbar.js
@@ -117,13 +117,13 @@ class Searchbar extends Component {
                     <button id="buttons" type="submit">
                         Submit
                     </button>
-                    <ul>
+                    <ul className="suggestions">
                         {suggestions.map(s => {
                             if (s.id === activeID) {
                                 return <li className="active" onClick={() => this.handleClick(s.text)} key={s.id}> {s.text} </li>
                             }
                             else {
-                                return <li onClick={() => this.handleClick(s.text)} key={s.id}> {s.text} </li>
+                                return <li className="inactive" onClick={() => this.handleClick(s.text)} key={s.id}> {s.text} </li>
                             }
                         })
                         }
