d8dc2f33e2efc6557e02abf4934ebb69581aedcd
leonardthang <leonard.thang@sjsu.edu>
2019-03-20 16:26:30 -0700

    Added product component

---
+++ client/src/components/common/Product.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Product = ({product}) => {
+    const { pName, price, weight, quantity } = product
+    return(
+        <div class="row">
+            <div class="col s12 m5">
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title">{pName}</span>
+                        <p>Price: {price}</p>
+                        <p>Weight: {weight}</p>
+                        <p>Quantity: {quantity}</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">Expand→ </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Product;
