234e7f246fed1fa5922f2ae8ea33f8ab86b8f114
Marissa <marxiong5@gmail.com>
2019-03-26 10:15:52 -0700

    Revert "whitebg"
    This reverts commit 8a046859395e96c773c814d6aa840527b9948dca.

--- client/src/itemholder.js
+++ client/src/itemholder.js
@@ -20,19 +20,19 @@ class Itemholder extends Component {
     const { products } = this.props
     return(
       <div class="row">
-        <div class="col s12 m5">
-          <div class="card">
-            <div class="card-content">
-              <span class="card-title">Item Name</span>
-              <p>Description</p>
-              <p>Price</p>
-            </div>
-          <div class="card-action">
-            <a href="#">Expand→ </a>
-          </div>
-        </div>
+  <div class="col s12 m6">
+    <div class="card blue-grey darken-1">
+      <div class="card-content white-text">
+        <span class="card-title">Item</span>
+        <p>Description</p>
+        <p>Price</p>
+      </div>
+      <div class="card-action">
+        <a href="#">Expand</a>
       </div>
     </div>
+  </div>
+</div>
     );
   }
 }
