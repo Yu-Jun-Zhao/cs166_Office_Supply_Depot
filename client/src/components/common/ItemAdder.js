c87a96d49d62b307b41dea402098e4fb3a2552ed
leonardthang <leonard.thang@sjsu.edu>
2019-04-16 19:57:36 -0700

    Added state for each field and an onchange handler. Added a dispatch for creating a product

--- client/src/components/common/ItemAdder.js
+++ client/src/components/common/ItemAdder.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+import {
+  createProduct
+} from "../../actions/productActions";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
@@ -9,40 +11,49 @@ import Grid from "@material-ui/core/Grid";
 // Mainly for Admin
 
 class ItemAdder extends Component {
-  constructor(props) {
-    super(props);
+
+  state = {
+    pName: null,
+    quantity: null,
+    price: null,
+    weight: null
   }
 
   componentDidMount() {}
 
+  setField = (e) => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+
   render() {
+    const { pName, quantity, price, weight } = this.state
     return (
-      <form>
+      <form onChange={this.setField}>
         <Grid container spacing={24}>
           <Grid item xs={6}>
             <TextField
               type="text"
               label="New Item"
-              id="new_item_text"
+              name="pName"
               fullWidth
             />
           </Grid>
 
           <Grid item xs={1}>
-            <TextField type="number" label="weight" id="new_item_weight" />
+            <TextField type="number" label="weight" name="weight" />
           </Grid>
 
           <Grid item xs={2}>
-            <TextField type="number" label="quantity" id="new_item_quantity" />
+            <TextField type="number" label="quantity" name="quantity" />
           </Grid>
 
           <Grid item xs={2}>
-            <TextField type="number" label="price" id="new_item_price" />
+            <TextField type="number" label="price" name="price" />
           </Grid>
 
           <Grid item xs={1}>
             <Button color="primary">
-              <i className="material-icons">add</i>
+              <i className="material-icons" onClick={() => this.props.dispatch(createProduct(pName, weight, quantity, price))}>add</i>
             </Button>
           </Grid>
         </Grid>
