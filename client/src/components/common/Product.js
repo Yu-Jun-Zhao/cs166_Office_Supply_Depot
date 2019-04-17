8d33008697cec6586dd9b41adab999b361c08a52
leonardthang <leonard.thang@sjsu.edu>
2019-04-16 22:46:51 -0700

    Removed materialize-css

--- client/src/components/common/Product.js
+++ client/src/components/common/Product.js
@@ -3,21 +3,13 @@ import React from "react";
 const Product = ({ product }) => {
   const { pName, price, weight, quantity } = product;
   return (
-    <div class="row">
-      <div class="col s12 m5">
-        <div class="card">
-          <div class="card-content">
-            <span class="card-title">{pName}</span>
-            <p>Price: {price}</p>
-            <p>Weight: {weight}</p>
-            <p>Quantity: {quantity}</p>
-          </div>
-          <div class="card-action">
-            <a href="#">Expand→ </a>
-          </div>
-        </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <span>{pName}</span>
+      <p>Price: {price}</p>
+      <p>Weight: {weight}</p>
+      <p>Quantity: {quantity}</p>
+      <hr/>
+    </React.Fragment>
   );
 };
 
