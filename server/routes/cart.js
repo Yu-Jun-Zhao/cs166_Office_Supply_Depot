db087c69d01c768ca94f4e9b72c92048a0315f43
Yu Jun Zhao <yuj.zhao4@gmail.com>
2019-04-25 14:42:50 -0700

    Added new function to server get cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -22,30 +22,50 @@ router.post("/add", (req, res) => {
 // @router GET /api/cart/all
 // @desc GET all items in cart
 // @access private
-/*
+
 router.get("/all", (req, res) => {
   const { cartId } = req.body;
   const sql = `SELECT cart_id, product_id, COUNT(*) AS quantity FROM cart_item WHERE cart_id = ${cartId} GROUP BY product_id`;
   pool.query(sql, (err, results) => {
-    if (err) res.send({ error: "Cannot fetch items from cart" });
-    const resObj = [];
-    for (var i = 0; i < results.length; i++) {
-      const nestedSQL = `SELECT p_name FROM product WHERE product_id = ${
-        results[i].product_id
-      }`;
-      pool.query(nestedSQL, (err, res) => {
-        if (err) return res.send({ error: "Cannot fetch this product info" });
-        resObj.push({
-          name: res.p_name,
-          quantity: results.quantity
-        });
-      });
-    }
-    return res.send({ resObj });
+    if (err) return res.send({ error: "Cannot fetch items from cart" });
+    getAllProductsName(res, results);
   });
 });
-*/
 
+// async function for fetching all products name
+async function getAllProductsName(res, outerSqlResults) {
+  var resObj = [];
+
+  try {
+    for (var i = 0; i < outerSqlResults.length; i++) {
+      const nestedSQL = `SELECT p_name FROM product WHERE product_id = ${
+        outerSqlResults[i].product_id
+      }`;
+      const productObj = await getProduct(nestedSQL, outerSqlResults[i]);
+      resObj.push(productObj);
+    }
+  } catch (err) {
+    resObj = [];
+  } finally {
+    res.send({ cart: resObj });
+  }
+}
+
+// Create the promise for fetching query
+function getProduct(query, outerSqlResult) {
+  return new Promise((resolve, reject) => {
+    pool.query(query, (err, result) => {
+      if (err) reject({ error: "Cannot fetch this product info" });
+      resolve({
+        id: outerSqlResult.product_id,
+        name: result[0].p_name,
+        quantity: outerSqlResult.quantity
+      });
+    });
+  });
+}
+
+/*
 router.get("/all", (req, res) => {
   const { cartId } = req.body;
   const sql = `SELECT cart_id, product_id, COUNT(*) AS quantity FROM cart_item WHERE cart_id = ${cartId} GROUP BY product_id`;
@@ -55,6 +75,7 @@ router.get("/all", (req, res) => {
     return res.send({ cart: results });
   });
 });
+*/
 
 //  @router DELETE /api/cart/
 //  @desc   Remove all items with the same product id from cart
