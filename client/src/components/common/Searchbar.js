a7a137fab3a60f8c7c39727dda304aa964f1e1d8
leonardthang <leonard.thang@sjsu.edu>
2019-04-28 18:28:05 -0700

    Removed search suggestions and redirect

--- client/src/components/common/Searchbar.js
+++ client/src/components/common/Searchbar.js
@@ -36,92 +36,16 @@ class Searchbar extends Component {
     suggestions: [],
     activeID: null,
     activeIdx: null,
-    toResults: false
   };
 
-  // Update search query and first 10 suggestions
-  handleChange = e => {
-    const { options } = this.state;
-    const value = e.target.value;
-    const suggestions = value ? options.get(value).slice(0, 10) : [];
-    this.setState({
-      searchQuery: value,
-      suggestions: suggestions,
-      activeID: null,
-      activeIdx: null
-    });
-  };
-  // Highlighting logic for the suggestions
-  handleKeyDown = e => {
-    const { suggestions, activeIdx, searchQuery, tempQuery } = this.state;
-    const len = suggestions.length;
-    if (e.currentTarget.value === "" || suggestions.length <= 0) return;
-    const kc = e.keyCode;
-    if (kc === 40 || kc === 38) {
-      e.preventDefault();
-      if (activeIdx === null) {
-        if (kc === 40) {
-          this.setState({
-            activeIdx: 0,
-            activeID: suggestions[0].id,
-            searchQuery: suggestions[0].text,
-            tempQuery: searchQuery
-          });
-        } else {
-          this.setState({
-            activeIdx: len - 1,
-            activeID: suggestions[len - 1].id,
-            searchQuery: suggestions[len - 1].text,
-            tempQuery: searchQuery
-          });
-        }
-        return;
-      }
-      const diff = kc === 40 ? 1 : -1;
-      const idx = activeIdx + diff;
-      if (idx >= len || idx < 0) {
-        this.setState({
-          activeIdx: null,
-          activeID: null,
-          searchQuery: tempQuery
-        });
-      } else {
-        this.setState({
-          activeIdx: idx,
-          activeID: suggestions[idx].id,
-          searchQuery: suggestions[idx].text
-        });
-      }
-      return;
-    }
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.setState({
-      toResults: true
-    });
-  };
-
-  handleClick = text => {
-    this.setState({
-      searchQuery: text,
-      toResults: true
-    });
-  };
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.history.push(`/result?q=${this.state.searchQuery}`)
+  }
 
   render() {
-    const { searchQuery, suggestions, activeID, toResults } = this.state;
+    const { searchQuery } = this.state;
     const { classes } = this.props;
-    if (toResults)
-      return (
-        <Redirect
-          to={{
-            pathname: "/result",
-            search: `q=${this.state.searchQuery}`
-          }}
-        />
-      );
 
     return (
       <div className="container">
@@ -151,48 +75,3 @@ class Searchbar extends Component {
 const SearchbarWithStyle = withStyles(styles)(Searchbar);
 
 export default withRouter(SearchbarWithStyle);
-
-/*
-<form onSubmit={this.handleSubmit}>
-        <div className="navigation">
-          <input
-            type="text"
-            value={searchQuery}
-            onChange={this.handleChange}
-            onKeyDown={this.handleKeyDown}
-            placeholder="Search..."
-          />
-          <button id="buttons" type="submit">
-            Submit
-          </button>
-          <ul className="suggestions">
-            {suggestions.map(s => {
-              if (s.id === activeID) {
-                return (
-                  <li
-                    className="active"
-                    onClick={() => this.handleClick(s.text)}
-                    key={s.id}
-                  >
-                    {" "}
-                    {s.text}{" "}
-                  </li>
-                );
-              } else {
-                return (
-                  <li
-                    className="inactive"
-                    onClick={() => this.handleClick(s.text)}
-                    key={s.id}
-                  >
-                    {" "}
-                    {s.text}{" "}
-                  </li>
-                );
-              }
-            })}
-          </ul>
-        </div>
-      </form>
-
-*/
