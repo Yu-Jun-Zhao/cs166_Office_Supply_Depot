8fc3a0e015ad291f43c298efce18e3687408da46
Yu Jun Zhao <yuj.zhao4@gmail.com>
2019-04-29 11:51:05 -0700

    testng

--- client/src/components/pages/OrderPage.js
+++ client/src/components/pages/OrderPage.js
@@ -1,52 +1,33 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withAuth } from "@okta/okta-react";
-import {checkAuthentication} from "../../actions/authenticateActions";
+//import { checkAuthentication } from "../../actions/authenticateActions";
 
 class OrderPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isAuthenticated: false,
-            name: ""
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isAuthenticated: false,
+      name: ""
+    };
+  }
 
-    componentDidMount() {
-        this.props.checkAuthentication(
-            this.props.auth,
-            this.props.authentication.isAuthenticated,
-            this.props.authentication.userInfo
-        );
-    }
+  componentDidMount() {}
 
-    static getDerivedStateFromProps(nextProps, prevState) {
-        if (nextProps.authentication.isAuthenticated) {
-            return {
-                isAuthenticated: true,
-                name: nextProps.authentication.userInfo.name
-            };
-        }
-        return {
-            isAuthenticated: false,
-            name: ""
-        };
-    }
-
-    render() {
-        return (
-            <React.Fragment>
-                <h1> TST </h1>
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <h1> TST </h1>
+      </React.Fragment>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    authentication: state.authentication
+  authentication: state.authentication
 });
 
 export default connect(
-    mapStateToProps,
-    { checkAuthentication }
-)(withAuth(OrderPage));
+  mapStateToProps,
+  {}
+)(OrderPage);
