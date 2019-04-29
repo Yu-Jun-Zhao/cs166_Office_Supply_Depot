ebc4b4c35416df30515c68617d01fcad8f125e9a
leonardthang <leonard.thang@sjsu.edu>
2019-04-28 18:29:55 -0700

    Removed trie dependency

--- client/src/components/common/Searchbar.js
+++ client/src/components/common/Searchbar.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import "../../style/homepage.css";
-import TrieSearch from "trie-search";
 import { withRouter } from "react-router-dom";
 import { withStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
