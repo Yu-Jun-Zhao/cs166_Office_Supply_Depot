9578a64ace78a73a2c929c8066e5e72bb26e422c
leonardthang <leonard.thang@sjsu.edu>
2019-04-29 09:22:52 -0700

    Added open and close actions

---
+++ client/src/actions/modalActions.js
@@ -0,0 +1,12 @@
+import {
+    OPEN_MODAL,
+    CLOSE_MODAL
+} from "./types";
+
+export const openModal = () => ({
+    type: OPEN_MODAL
+});
+
+export const closeModal = () => ({
+    type: CLOSE_MODAL
+});
