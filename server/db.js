e6996d4b940b3f5aafc6c914475fa2c34f31f6c6
Yu Jun Zhao <yuj.zhao4@gmail.com>
2019-05-01 17:22:59 -0700

    testing and checkign

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ import mysql from "mysql";
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: "localhost",
-  user: "root",
-  password: "",
+  user: "user",
+  password: "1234@bcd",
   database: "osd",
   port: 3306
 });
