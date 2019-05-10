1049f43407d562724039bb416908bd31a81c9d65
leonardthang <leonard.thang@sjsu.edu>
2019-05-09 17:57:53 -0700

    Remove outdated materialize class

--- client/src/components/common/Product.js
+++ client/src/components/common/Product.js
@@ -3,11 +3,11 @@ import React from "react";
 const Product = ({ product }) => {
   const { p_name, price, weight, quantity } = product;
   return (
-    <div class="row">
-      <div class="col s12 m5">
-        <div class="card">
-          <div class="card-content">
-            <span class="card-title">{p_name}</span>
+    <div>
+      <div>
+        <div>
+          <div>
+            <span >{p_name}</span>
             <p>Price: {price}</p>
             <p>Weight: {weight}</p>
             <p>Quantity: {quantity}</p>
