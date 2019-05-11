b97e7791845d07c7e92773ad846caddcbb75d3ac
leonardthang <leonard.thang@sjsu.edu>
2019-05-10 23:30:19 -0700

    Fixed bug 1 from team 5

--- client/src/components/main/CommonPage.js
+++ client/src/components/main/CommonPage.js
@@ -1,27 +1,9 @@
 import React, { Component } from "react";
 import {
-  IconButton,
-  Drawer,
-  List,
-  ListItem,
-  ListItemText,
-  Fab,
-  Divider,
-  ListItemSecondaryAction,
-  Switch,
-  ListItemIcon,
-  Collapse,
-  TextField,
   Grid
 } from "@material-ui/core/";
 
 import ReactPaginate from "react-paginate";
-import MenuIcon from "@material-ui/icons/Menu";
-import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
-import ExpandLessIcon from "@material-ui/icons/ExpandLess";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-
 import "../../index.css";
 import { withStyles } from "@material-ui/core/styles";
 
@@ -33,9 +15,6 @@ import ItemCard from "../common/ItemCard";
 import SimpleModal from "../common/SimpleModal";
 
 const drawerWidth = "230px";
-
-// for testing
-const arrayLoop = [...new Array(16)];
 
 const styles = theme => ({
   root: {
@@ -100,22 +79,7 @@ class CommonPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drawerOpen: true,
       selection: props.selection,
-      color: props.color,
-      label: props.label,
-      currentPage: 1,
-      //totalPage: 0,
-      //loading: false,
-      //data: [],
-
-      // not supported
-      filterExpand: ["price"], //expand by default
-      filterApply: [],
-      priceRange: {
-        min: 0,
-        max: 99999
-      }
     };
   }
 
@@ -123,153 +87,18 @@ class CommonPage extends Component {
     this.props.fetchProductByType(this.state.selection);
   }
 
-  //not applicable
-  handleDrawerOpen = () => {
-    this.setState({ drawerOpen: true });
-  };
-
-  //not applicable
-  handleDrawerClose = () => {
-    this.setState({ drawerOpen: false });
-  };
-
-  //not applicable
-  // A function that takes in an array (filterExpand or filterApply) and a value and returns a function
-  // To keep track of what filter is applied
-  handleArrayChange = (array, value) => () => {
-    const index = array.indexOf(value);
-
-    // Not in array
-    if (index === -1) {
-      array.push(value);
-    } else {
-      array.splice(index, 1);
-    }
-
-    this.setState({
-      array
-    });
-  };
-
-  // not applicable
-  handlePriceChange = name => event => {
-    const value = event.target.value;
-    this.setState(prevState => ({
-      priceRange: {
-        ...prevState.priceRange,
-        [name]: value
-      }
-    }));
-  };
-
   render() {
-    const {
-      drawerOpen,
-      color,
-      label,
-      filterExpand,
-      filterApply
-      //totalPage,
-      //loading,
-      //data
-    } = this.state;
     const { classes, products } = this.props;
-    const rootDrawer = false // drawerOpen === false
-      ? classes.drawerRootBig
-      : classes.drawerRootSmall;
+
+    if (!products) return <div> LOADING... </div>;
 
     return (
       <div className={classes.root}>
         <SimpleModal/>
-        <Drawer
-          open={false} // TODO: for now it will stay false
-          variant="persistent"
-          className={classes.drawer}
-          classes={{ paper: classes.drawerPaper, root: rootDrawer }}
-          elevation={6}
-          anchor="left"
-        >
-          <div className={classes.drawerArrowButton}>
-            <IconButton onClick={this.handleDrawerClose}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </div>
-          <Divider />
-          <List classes={{ root: classes.listRoot }}>
-            <ListItem
-              button
-              onClick={this.handleArrayChange(filterExpand, "price")}
-            >
-              <ListItemIcon style={{ marginRight: "0px" }}>
-                <AttachMoneyIcon />
-              </ListItemIcon>
-              <ListItemText
-                primary="Price Range"
-                classes={{ root: classes.listItemTextRoot }}
-              />
-              <ListItemIcon style={{ marginLeft: "15%" }}>
-                {filterExpand.indexOf("price") !== -1 ? (
-                  <ExpandLessIcon />
-                ) : (
-                  <ExpandMoreIcon />
-                )}
-              </ListItemIcon>
-            </ListItem>
-            <Collapse
-              in={filterExpand.indexOf("price") !== -1}
-              timeout="auto"
-              unmountOnExit
-            >
-              <List disablePadding>
-                <ListItem className={classes.listItemRoot}>
-                  <TextField
-                    type="number"
-                    margin="normal"
-                    className={classes.textField}
-                    placeholder="$"
-                    disabled={filterApply.indexOf("price") === -1}
-                    onChange={this.handlePriceChange("min")}
-                  />
-                  <ListItemText
-                    primary="to"
-                    classes={{ root: classes.listItemTextRoot }}
-                  />
-                  <TextField
-                    type="number"
-                    margin="normal"
-                    className={classes.textField}
-                    placeholder="$"
-                    disabled={filterApply.indexOf("price") === -1}
-                    onChange={this.handlePriceChange("max")}
-                  />
-                  <ListItemSecondaryAction>
-                    <Switch
-                      onChange={this.handleArrayChange(filterApply, "price")}
-                      checked={filterApply.indexOf("price") !== -1}
-                    />
-                  </ListItemSecondaryAction>
-                </ListItem>
-              </List>
-            </Collapse>
-          </List>
-        </Drawer>
-
-        {/*(drawerOpen) True for now but Function not supported*/}
-        {false && (
-          <Fab
-            classes={{ root: classes.fab }}
-            color="secondary"
-            size="small"
-            aria-label="Filter Menu"
-            onClick={this.handleDrawerOpen}
-          >
-            <MenuIcon />
-          </Fab>
-        )}
 
         <main className={classes.content}>
           <Grid container spacing={24}>
-            {products.items && products.items.map((item, index) => (
+            {products && products.map((item, index) => (
               <Grid item xs={3} key={index}>
                 <ItemCard
                   id={item.product_id}
@@ -308,7 +137,7 @@ class CommonPage extends Component {
 const CommonPageStyled = withStyles(styles)(CommonPage);
 
 const mapStateToProps = state => ({
-  products: state.products
+  products: state.products.items
 });
 
 export default connect(
